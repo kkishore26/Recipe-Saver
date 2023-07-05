a2de1e4baf6fcac1a035255722695c31639f42b0
Kamal Kishore <kkishore5943@gmail.com>
2023-07-05 19:37:06 +0530

    First Commit
    First Date : 05/06/2023

---
+++ home/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from .models import *
+
+# Create your views here.
+
+def recipes(request):
+
+    if request.method == "POST":
+        data = request.POST
+        recipe_name = data.get('recipe_name')
+        recipe_description = data.get('recipe_description')
+        recipe_image = request.FILES.get('recipe_image')
+
+        Recipe.objects.create(
+            recipe_name = recipe_name,
+            recipe_description = recipe_description,
+            recipe_image = recipe_image,
+            )
+        
+        return redirect('recipes')
+    
+    queryset = Recipe.objects.all()
+    context = {'recipes' : queryset}
+    return render(request , 'recipes.html' , context)
+
+def delete_recipe(request, id):
+    queryset = Recipe.objects.get(id=id)
+    return redirect('recipes')
